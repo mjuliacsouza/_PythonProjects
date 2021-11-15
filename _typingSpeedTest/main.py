a227df7eefa945bf016f2ed88bd10747dbb7a9b6
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-11-15 11:37:44 -0300

    :tada: estudos 15-11

--- _typingSpeedTest/main.py
+++ _typingSpeedTest/main.py
@@ -2,9 +2,14 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
     stdscr.clear()
-    stdscr.addstr("Hello world!")
+    stdscr.addstr(1, 0, "Hello world!")
     stdscr.refresh()
-    stdscr.getkey()
+    key = stdscr.getkey()
+    print(key)
 
 wrapper(main)

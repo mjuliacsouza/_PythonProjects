ebd0fc62f6029c9dcb8337a4f2c495bb9f5ea154
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-11-15 12:25:28 -0300

    :tada: estudos 15-11

--- _typingSpeedTest/main.py
+++ _typingSpeedTest/main.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import time
+import random
 
 
 def start_screen(stdscr):
@@ -25,8 +26,14 @@ def display_text(stdscr, target, current, wpm=0):
         stdscr.addstr(0, i, char, color)
 
 
+def load_text():
+    with open("text.txt", "r") as file:
+        lines = file.readlines()
+        return random.choice(lines).strip()
+
+
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = load_text()
     current_text = []
     start_time = time.time()
     stdscr.nodelay(True)
